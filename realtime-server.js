33dc11aa74fff75db8274b64b2f798a1a7feabc7
Karthik K <imkarthikk@users.noreply.github.com>
2016-04-22 23:34:21 +0530

    Update realtime-server.js

--- realtime-server.js
+++ realtime-server.js
@@ -1,5 +1,5 @@
 var environment = require('./environment.js');
-var redis = environment.loadRedis("redis://127.0.0.1:6379/0", "5321fa04918d32c4bb939a5fc313df21946a344fa58006615e936b4382a1a3bd");
+var redis = environment.loadRedis("redis://127.0.0.1:6379/0", "pRejaswu4acruxumebumaprecHabraph");
 var io = environment.loadSocketIo(redis);
 
 environment.authorize(io, redis);
